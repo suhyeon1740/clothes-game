020fe799b65355296c378c6b7fdded736ae1a772
Elly (박수현) <suhyeon1740@gmail.com>
2021-05-28 00:18:48 +0900

    시간이미지 추가

---
+++ Assets/Script/Timer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    public TextMeshProUGUI timeLable;
+    private int Time = 30;
+
+    // private IEnumerator
+}
