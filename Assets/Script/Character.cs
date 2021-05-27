bc8fd3544e7ae5cd880a3b767bd526957f998312
Elly (박수현) <suhyeon1740@gmail.com>
2021-05-27 13:36:39 +0900

    옷 이미지 추가 및 배치

--- Assets/Script/Character.cs
+++ Assets/Script/Character.cs
@@ -1,10 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; // Image사용을 위해서는 UnityEngine.UI를 사용해야함
 
 public class Character : MonoBehaviour
 {
     public RectTransform rt;
+    public Image shirt;
+
+    public void ChangeShirt(Sprite image)
+    {
+        shirt.enabled = true; // 처음에 흰 이미지때문에 enabled 해제해준거 다시 적용
+        shirt.sprite = image;
+        shirt.SetNativeSize();
+    }
     public void LeftButtonClick()
     {
         if (rt.localScale.x < 0)
