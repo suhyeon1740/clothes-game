49ef16b632301c1eaed138360b7fa5c06db4e50d
Elly (박수현) <suhyeon1740@gmail.com>
2021-05-27 21:22:57 +0900

    결과버튼구현

---
+++ Assets/Script/ResultButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResultButton : MonoBehaviour
+{
+    public Character character;
+    string answerShirtName = "character_shirt1";
+
+    public void checkAnswer()
+    {
+        // 옷을 안입었거나 정답과 다르면
+        if (character.shirt.sprite == null ||
+            !answerShirtName.Equals(character.shirt.sprite.name))
+        {
+            Debug.Log("셔츠가 틀렸습니다");
+        }
+    }
+}
