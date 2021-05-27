aa4e3e2e9272edf86efe7455833060c6fa10bc84
Elly (박수현) <suhyeon1740@gmail.com>
2021-05-28 06:58:29 +0900

    타이머 구현

--- Assets/Script/Timer.cs
+++ Assets/Script/Timer.cs
@@ -6,7 +6,21 @@ using UnityEngine;
 public class Timer : MonoBehaviour
 {
     public TextMeshProUGUI timeLable;
-    private int Time = 30;
+    private int time = 30;
 
-    // private IEnumerator
+    // start: 오브젝트 생성 후 처음 한번 자동으로 실행되는 함수
+    private void Start() {
+        StartCoroutine(DoTime());
+    }
+    private IEnumerator DoTime()
+    {
+        yield return new WaitForSeconds(1.0f); // 1초 대기 후 아래 코드 실행
+        time--;
+        timeLable.text = time.ToString();
+        if (time > 0) // 시간 예외처리
+        {
+            StartCoroutine(DoTime());
+        }
+
+    }
 }
