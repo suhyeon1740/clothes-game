71e74a6df686e6513d96c01f252e6daac49f130f
Elly (박수현) <suhyeon1740@gmail.com>
2021-05-27 13:12:03 +0900

    캐릭터 회전

---
+++ Assets/Script/Character.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public RectTransform rt;
+    public void LeftButtonClick()
+    {
+        if (rt.localScale.x < 0)
+        {
+            rt.localScale = new Vector2(rt.localScale.x * -1f, rt.localScale.y);
+        }
+    }
+    public void RightButtonClick()
+    {
+        if (rt.localScale.x > 0)
+        {
+            rt.localScale = new Vector2(rt.localScale.x * -1f, rt.localScale.y);
+        }
+    }
+}
